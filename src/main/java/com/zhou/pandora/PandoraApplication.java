50f6b4dac679543afc13605862b832697f4000a4
zhouhonggang <honggang_z@yeah.net>
2021-05-12 10:34:31 +0800

    应用初始化

---
+++ src/main/java/com/zhou/pandora/PandoraApplication.java
@@ -0,0 +1,13 @@
+package com.zhou.pandora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhou/pandora/PandoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhou.pandora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
